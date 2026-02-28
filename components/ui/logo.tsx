433528484880027fa27ce10457673fd4439ff9b6
condyl <Condyl678@gmail.com>
2026-02-28 12:33:22 -0500

    refactor: update DiscordButton to use Button component and improve Logo with Next.js Image

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export function Logo() {
   return (
     <div className="flex items-center gap-3 cursor-pointer">
@@ -5,10 +7,12 @@ export function Logo() {
         className="flex items-center justify-center w-10 h-10 bg-[#9A4C46] border-[2px] border-black rounded-[5px]"
         style={{ boxShadow: '3px 3px 0px 0px rgba(0,0,0,1)' }}
       >
-        <img 
-          src="/logo-light.svg" 
-          alt="Badger Logo" 
-          className="w-[90%] h-[85%] object-contain" 
+        <Image
+          src="/logo-light.svg"
+          alt="Badger Logo"
+          width={36}
+          height={34}
+          className="object-contain"
         />
       </div>
     </div>
