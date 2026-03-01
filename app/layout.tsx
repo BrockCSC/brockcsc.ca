9182f2c5be73f354a6d9cf236a3e94ed84f1269c
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2026-03-01 14:37:26 -0500

    added footer component to layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
 import "./globals.css";
 import { Navbar } from "@/components/ui/navbar";
-
-
+import Footer from "@/components/ui/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +39,7 @@ export default function RootLayout({
           <Navbar />
           <div className="mx-auto w-full max-w-[1060px] px-5">{children}</div>
         </div>
+        <Footer />
       </body>
     </html>
   );
