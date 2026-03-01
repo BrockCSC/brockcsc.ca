b39bd3e5837965e0a4096d16ba1ca2d4db90f06a
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2026-03-01 14:29:40 -0500

    Created the Footer Component and Removed the Hover Animation on Card Component (#34)
    * created card wrapper component
    
    * removed the data interface
    
    * removed the hover animation so card component is static
    
    * created footer component
    
    * added in logo for footer component

---
+++ components/ui/footer.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/logo.svg";
+
+export default function Footer() {
+  return (
+    <div
+      className="flex items-center justify-around gap-4 bg-foreground p-12  
+          shadow-[0px_-4px_0px_var(--color-primary)]"
+    >
+      <Link href="/">
+        <Image src={logo} alt="Brock CSC Logo" width={80} height={80} />
+      </Link>
+      <div className="flex flex-col gap-1">
+        <h1 className="text-primary text-xl font-extrabold">BROCK CSC</h1>
+        <p className="text-gray-500 text-sm">
+          © 2023 Brock Computer Science - All Rights Reserved
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href="https://www.instagram.com/brockcsc/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <p className="text-gray-500 text-sm">Instagram</p>
+        </Link>
+        <Link
+          href="https://discord.com/invite/qsctEK2"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <p className="text-gray-500 text-sm">Discord</p>
+        </Link>
+        <Link
+          href="https://www.linkedin.com/company/brockcsc"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <p className="text-gray-500 text-sm">LinkedIn</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
